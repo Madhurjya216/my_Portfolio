e7c143f909c45650c94c89df356d7051da098eff
Madurjya kashyap <madhurjyakashyap216@gmail.com>
2023-06-06 22:17:35 +0530

    made changes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const topBtn = document.getElementById('to_btn');
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) { 
     topBtn.style.display = "block";
   } else {
     topBtn.style.display = "none";
